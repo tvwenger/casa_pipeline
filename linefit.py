b6f0a84cd91d6fa8c27fa9f352092b14c4fa4cf9
Trey Wenger <tvwenger@gmail.com>
2017-05-12 14:23:35 +1000

    added linefit

---
+++ linefit.py
@@ -0,0 +1,172 @@
+"""
+linefit.py
+CASA Data Reduction Pipeline - Line fitting script
+Trey V. Wenger May 2017 - V1.0
+"""
+
+import __main__ as casa # import casa namespace
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+__VERSION__ = "1.0"
+
+def gaussians(x,amps,fwhms,centers,cont):
+    sigmas = fwhms / (2.*np.sqrt(2.*np.log(2.)))
+    y = np.sum([a*np.exp(-(x-c)**2./(2*s**2.))
+                for a,s,c in zip(amps,sigmas,centers)],axis=0)
+    y = y + cont
+    return y
+
+class ClickPlot:
+    """
+    Generic class for generating and interacting with matplotlib figures
+    """
+    def __init__(self,num):
+        self.fig = plt.figure(num)
+        plt.clf()
+        print "Created figure",self.fig.number
+        self.ax = self.fig.add_subplot(111)
+        self.clickbutton = []
+        self.clickx_data = []
+        self.clicky_data = []
+
+    def onclick(self,event):
+        """
+        Handle click event
+        """
+        if event.button not in [1,3]:
+            return
+        try:
+            print "Click at ({0:.2f},{1:.2f})".format(event.xdata,
+                                                      event.ydata)
+        except ValueError:
+            return
+        self.clickbutton.append(event.button)
+        self.clickx_data.append(event.xdata)
+        self.clicky_data.append(event.ydata)
+
+    def get_line_free_regions(self,xdata,ydata,xlabel=None,ylabel=None):
+        """
+        Using click events to get the line free regions of a
+        spectrum
+        """
+        self.ax.clear()
+        self.ax.plot(xdata,ydata,'k-')
+        self.ax.set_xlabel(xlabel)
+        self.ax.set_ylabel(ylabel)
+        self.clickbutton = []
+        self.clickx_data = []
+        self.clicky_data = []
+        print "Left click to select start of line-free-region."
+        print "Left click again to select end of line-free-region."
+        print "Repeat as necessary."
+        print "Right click when done."
+        cid = self.fig.canvas.mpl_connect('button_press_event',
+                                          self.onclick)
+        self.fig.show()
+        while True:
+            self.fig.waitforbuttonpress()
+            if 3 in self.clickbutton:
+                break
+            elif 1 in self.clickbutton:
+                self.ax.axvline(self.clickx_data[-1])
+        self.fig.canvas.mpl_disconnect(cid)
+        # remove last element (right-click)
+        self.clickx_data = self.clickx_data[0:-1]
+        # check that there are an even number, otherwise remove last
+        # element
+        if len(self.clickx_data) % 2 != 0:
+            self.clickx_data = self.clickx_data[0:-1]
+        regions = zip(self.clickx_data[::2],self.clickx_data[1::2])
+        return regions
+
+    def get_gauss_estimates(self,xdata,ydata,xlabel=None,ylabel=None):
+        """
+        Using click events to get the gaussian fit estimates
+        """
+        self.ax.clear()
+        self.ax.plot(xdata,ydata,'k-')
+        self.ax.set_xlabel(xlabel)
+        self.ax.set_ylabel(ylabel)
+        self.clickbutton = []
+        self.clickx_data = []
+        self.clicky_data = []
+        print "Left click to select start of Gaussian region"
+        print "For each Gaussian: Left click to select peak of Gaussian"
+        print "For each Gaussian: Left click to select half-maximum of Gaussian"
+        print "Left click to select end of Gaussian region"
+        print "Right click when done."
+        cid = self.fig.canvas.mpl_connect('button_press_event',
+                                          self.onclick)
+        self.fig.show()
+        while True:
+            self.fig.waitforbuttonpress()
+            if 3 in self.clickbutton:
+                break
+            elif 1 in self.clickbutton:
+                self.ax.axvline(self.clickx_data[-1])
+        self.fig.canvas.mpl_disconnect(cid)
+        # remove last element (right-click)
+        self.clickx_data = self.clickx_data[0:-1]
+        # check that there are an even number, otherwise remove last
+        # element
+        if len(self.clickx_data) % 2 != 0:
+            self.clickx_data = self.clickx_data[0:-1]
+        start = self.clickx_data[0]
+        centers = self.clickx_data[1:-1:2]
+        widths = self.clickx_data[2:-1:2]
+        end = self.clickx_data[-1]
+        return (start,centers,widths,end)
+
+def fit(imagename,region):
+    """
+    Fit Gaussian(s) to spectrum extracted from image within a region
+
+    Inputs:
+      imagename  = CASA image file to fit
+      region = CASA region file
+
+    Returns:
+      (peaks, centers, fwhms, continuum, rms)
+    """
+    stats = casa.imstat(imagename=imagename,region=region,axes=[0,1])
+    myplot = ClickPlot(0)
+    chans = range(len(stats['max']))
+    fluxes =  stats['max']
+    #
+    # Get line-free regions
+    #
+    regs = myplot.get_line_free_regions(chans,fluxes,
+                                        xlabel='channel',
+                                        ylabel='peak flux density (Jy/beam)')
+    fluxdata = np.array([])
+    for idx,reg in enumerate(regs):
+        xstart = chans[np.argmin(np.abs(reg[0]-chans))]
+        xend = chans[np.argmin(np.abs(reg[1]-chans))]
+        fluxdata = np.append(fluxdata,fluxes[xstart:xend])
+    rms = np.sqrt(np.mean((fluxdata-np.mean(fluxdata))**2.))
+    #
+    # Fit-gaussians
+    #
+    estimates = myplot.get_gauss_estimates(chans,fluxes,
+                                           xlabel='channel',
+                                           ylabel='peak flux density (Jy/beam)')
+    start, centers, widths, end = estimates
+    start_ind = np.argmin(np.abs(start-chans))
+    end_ind = np.argmin(np.abs(end-chans))
+    center_inds = np.array([np.argmin(np.abs(c-chans)) for c in centers])
+    width_inds = np.array([np.argmin(np.abs(w-chans)) for w in widths])
+    xdata = chans[start_ind:end_ind]
+    ydata = fluxes[start_ind:end_ind]
+    p0_amps = ydata[center_inds]
+    p0_fwhms = xdata[width_inds]-xdata[center_inds]
+    p0_centers = xdata[center_inds]
+    p0_cont = np.mean(fluxdata)
+    popt, pcov = curve_fit(gaussians,xdata,ydata,
+                           p0=(p0_amps,p0_centers,p0_fwhms,p0_cont))
+    #
+    # Return results
+    #
+    return (popt[0],popt[1],popt[2],popt[3],rms)
